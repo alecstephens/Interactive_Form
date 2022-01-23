c0dc66fd816cad444d65f58f9a7f4205bd911ad5
alecstephens <stephens.alec01@gmail.com>
2022-01-23 11:21:57 -0600

    Began adding some functions and event listeners

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
+function getFocus() {
+    document.getElementById('name').focus();
+}
+getFocus();
+
+
+const otherJobRole = document.getElementById('other-job-role');
+otherJobRole.style.display = 'none';
+
+const select = document.querySelector('select');
+select.addEventListener('change', () => {
+    if(select.value === 'other') {
+        otherJobRole.style.display = 'block';
+    } else {
+        otherJobRole.style.display = 'none';
+    }
+});
+
+const color = document.getElementById('color');
+color.disabled = true;
+const design = document.getElementById('design');
+design.addEventListener('change', () => {
+    color.disabled = false;
+});
 console.log('test');
