2e8058b41342468d17db3c68aafeedadf96365c5
alecstephens <stephens.alec01@gmail.com>
2022-01-24 10:28:06 -0600

    Edited and added some event listeners

--- js/script.js
+++ js/script.js
@@ -19,7 +19,33 @@ select.addEventListener('change', () => {
 const color = document.getElementById('color');
 color.disabled = true;
 const design = document.getElementById('design');
-design.addEventListener('change', () => {
+
+design.addEventListener('change', (e) => {
     color.disabled = false;
+    const colorOptions = color.children;
+
+    for(let i = 0; i < colorOptions.length; i++) {
+        let colorOptionsValue = e.target.value;
+        let dataTheme = colorOptions[i].getAttribute('data-theme');
+
+        if(colorOptionsValue === dataTheme) {
+            colorOptions[i].hidden = false;
+        } else {
+            colorOptions[i].hidden = true;
+        }
+    }
 });
-console.log('test');
+
+const activities = document.getElementById('activities');
+const activitiesCost = document.getElementById('activities-cost');
+let totalPrice = 0;
+
+activities.addEventListener('change', (e) => {
+    let cost = +e.target.getAttribute('data-cost');
+    if(e.target.checked) {
+        totalPrice += cost;
+    } else {
+        totalPrice -= cost;
+    }
+    activitiesCost.innerText = `Total: $${totalPrice}`;
+});
