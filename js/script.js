ee81419eb327a86822bcf33bdb1aad03d07ccc7f
alecstephens <stephens.alec01@gmail.com>
2022-01-25 09:42:23 -0600

    Added another event listener and added comments for better understanding

--- js/script.js
+++ js/script.js
@@ -20,6 +20,10 @@ const color = document.getElementById('color');
 color.disabled = true;
 const design = document.getElementById('design');
 
+/*
+  Event listener that disables or enables colors 
+  depeding on what shirt design is picked
+*/
 design.addEventListener('change', (e) => {
     color.disabled = false;
     const colorOptions = color.children;
@@ -36,10 +40,13 @@ design.addEventListener('change', (e) => {
     }
 });
 
+// Grabs some elements in the activities section
 const activities = document.getElementById('activities');
 const activitiesCost = document.getElementById('activities-cost');
 let totalPrice = 0;
 
+
+// Adds up the cost of all of the activities picked
 activities.addEventListener('change', (e) => {
     let cost = +e.target.getAttribute('data-cost');
     if(e.target.checked) {
@@ -49,3 +56,49 @@ activities.addEventListener('change', (e) => {
     }
     activitiesCost.innerText = `Total: $${totalPrice}`;
 });
+
+// Grabs all of the elements from the payment section
+const payment = document.getElementById('payment');
+const creditCard = document.getElementById('credit-card');
+const payPal = document.getElementById('paypal');
+const bitCoin = document.getElementById('bitcoin');
+
+payPal.style.display = 'none';
+bitCoin.style.display = 'none';
+payment.value = 'credit-card';
+
+/*
+  Event listener for displaying which payment option is 
+  selected and will disable the ones not selected
+*/
+payment.addEventListener('change', (e) => {
+    if(e.target.value === 'credit-card') {
+        creditCard.style.display = 'block';
+        payPal.style.display = 'none';
+        bitCoin.style.display = 'none';
+    } else if(e.target.value === 'paypal') {
+        creditCard.style.display = 'none';
+        payPal.style.display = 'block';
+        bitCoin.style.display = 'none';
+    } else if(e.target.value === 'bitcoin') {
+        creditCard.style.display = 'none';
+        payPal.style.display = 'none';
+        bitCoin.style.display = 'block';
+    }
+});
+
+// Elements for form validation
+const email = document.getElementById('email');
+const cardNumber = document.getElementById('cc-num');
+const zip = document.getElementById('zip');
+const cv = document.getElementById('cvv');
+const form = document.querySelector('form');
+
+
+// Form validation functions
+function validateName() {
+    const nameInput = nameField.value;
+    
+}
+
+
