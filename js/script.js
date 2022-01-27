bfc21826d0fd3dfb7bf12e39f309105892798641
alecstephens <stephens.alec01@gmail.com>
2022-01-27 13:16:28 -0600

    Finished the form validation portion

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
-function getFocus() {
-    document.getElementById('name').focus();
-}
-getFocus();
-
+const nameField = document.getElementById('name');
+nameField.focus();
 
 const otherJobRole = document.getElementById('other-job-role');
 otherJobRole.style.display = 'none';
@@ -91,14 +88,107 @@ payment.addEventListener('change', (e) => {
 const email = document.getElementById('email');
 const cardNumber = document.getElementById('cc-num');
 const zip = document.getElementById('zip');
-const cv = document.getElementById('cvv');
+const cvv = document.getElementById('cvv');
 const form = document.querySelector('form');
 
 
 // Form validation functions
 function validateName() {
-    const nameInput = nameField.value;
-    
+    let nameInput = nameField.value;
+    let validName = /^[a-z]/i.test(nameInput);
+    if(!validName) {
+        nameField.parentElement.classList.add('not-valid');
+        nameField.parentElement.lastElementChild.style.display = 'block';
+    } else {
+        nameField.parentElement.classList.replace('not-valid', 'valid');
+        return true;
+    }
 }
+
+function validateEmail() {
+    let emailInput = email.value;
+    let validEmail = /^[^@]+@[^@.]+\.[a-z]+$/i.test(emailInput);
+    if(!validEmail) {
+        email.parentElement.classList.add('not-valid');
+        email.parentElement.lastElementChild.style.display = 'block';
+    } else {
+        email.parentElement.classList.replace('not-valid', 'valid');
+        return true;
+    }
+}
+
+function validateActivities() {
+    if(totalPrice === 0){
+        activities.classList.add('not-valid');
+        activities.parentElement.lastElementChild.style.display = 'block';
+    } else {
+        activities.classList.replace('not-valid', 'valid');
+        return true;
+    }
+}
+
+function validateCard() {
+    let cardInput = cardNumber.value;
+    let validCard = /^\d{13,16}$/.test(cardInput);
+    if(!validCard) {
+        cardNumber.parentElement.classList.add('not-valid');
+        cardNumber.parentElement.lastElementChild.style.display = 'block';
+    } else {
+        cardNumber.parentElement.classList.replace('not-valid', 'valid');
+        return true;
+    }
+}
+
+function validateZip () {
+    let zipInput = zip.value;
+    let validZip = /^\d{5}$/.test(zipInput);
+    if(!validZip) {
+        zip.parentElement.classList.add('not-valid');
+        zip.parentElement.lastElementChild.style.display = 'block';
+    } else {
+        zip.parentElement.classList.replace('not-valid', 'valid');
+        return true;
+    }
+}
+
+function validateCvv() {
+    let cvvInput = cvv.value;
+    let validCvv = /^\d{3}$/.test(cvvInput);
+    if(!validCvv) {
+        cvv.parentElement.classList.add('not-valid');
+        cvv.parentElement.lastElementChild.style.display = 'block';
+    } else {
+        cvv.parentElement.classList.replace('not-valid', 'valid');
+        return true;
+    }
+}
+
+/* 
+  Event listener that activates when a user submits
+  the form and runs the functions that check whether
+  or the not the information given is valid. Then it prevents
+  the form from submitting if it is not valid.
+*/
+form.addEventListener('submit', (e) => {
+    if(!validateName()) {
+        e.preventDefault();
+    }
+    if(!validateEmail()) {
+        e.preventDefault();
+    }
+    if(!validateActivities()) {
+        e.preventDefault();
+    }
+    if(!validateCard()) {
+        e.preventDefault();
+    }
+    if(!validateZip()) {
+        e.preventDefault();
+    }
+    if(!validateCvv()) {
+        e.preventDefault();
+    }
+});
+
 
 
